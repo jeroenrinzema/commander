7454a0068e3abafd7a2215480e720bee7d5c0efc
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-07-24 17:11:52 +0200

    refactoring: KeyAcknowledged -> AcknowledgedHeader

--- event.go
+++ event.go
@@ -18,36 +18,53 @@ type Event struct {
 	Acknowledged bool            `json:"acknowledged"`
 }
 
+var handlerMap map[string]func(event *Event, headerValue []byte) error{
+	ActionHeader: HandleActionHeader,
+	ParentHeader: HandleParentHeader,
+	AcknowledgedHeader: HandleAcknowledgedHeader,
+	KeyHeader: HandleKeyHeader,
+}
+
+func HandleActionHeader(event *Event, headerValue []byte) error {
+	event.Action = string(headerValue)
+	return nil
+}
+
+func HandleParentHeader(event *Event, headerValue []byte) error {
+	parent, err := uuid.FromBytes(headerValue)
+	if err != nil {
+		return err
+	}
+	event.Parent = parent
+	return nil
+}
+
+func HandleAcknowledgedHeader(event *Event, headerValue []byte) error {
+	acknowledged, err := strconv.ParseBool(string(header.Value))
+	if err != nil {
+		return err
+	}
+	event.Acknowledged = acknowledged
+	return nil
+}
+
+func HandleKeyHeader(event *Event, headerValue []byte) error {
+	key, err := uuid.FromBytes(header.Value)
+	if err != nil {
+		return err
+	}
+	event.Key = key
+	return nil
+}
+
 // Populate the event with the data from the given kafka message
 func (event *Event) Populate(message *kafka.Message) error {
 	for _, header := range message.Headers {
-		switch header.Key {
-		case ActionHeader:
-			event.Action = string(header.Value)
-		case ParentHeader:
-			parent, err := uuid.FromBytes(header.Value)
-
+		if v, ok := handlerMap[header.Key]; ok {
+			err := v()
 			if err != nil {
 				return err
 			}
-
-			event.Parent = parent
-		case KeyHeader:
-			key, err := uuid.FromBytes(header.Value)
-
-			if err != nil {
-				return err
-			}
-
-			event.Key = key
-		case KeyAcknowledged:
-			acknowledged, err := strconv.ParseBool(string(header.Value))
-
-			if err != nil {
-				return err
-			}
-
-			event.Acknowledged = acknowledged
 		}
 	}
 

7ccd5bb196f320beb3030bd876ac5abd4059d156
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-09-12 23:06:08 +0200

    feat: events and commands

--- command.go
+++ command.go
@@ -3,89 +3,13 @@ package commander
 import (
 	"encoding/json"
 
-	"github.com/Shopify/sarama"
 	uuid "github.com/satori/go.uuid"
 )
 
-// NewCommand create a new command with the given action and data.
-// A unique ID is generated and set in order to trace the command.
-func NewCommand(action string, data []byte) *Command {
-	id := uuid.NewV4()
-
-	command := Command{
-		ID:     id,
-		Action: action,
-		Data:   data,
-	}
-
-	return &command
-}
-
-// Command a command contains a order for a data change
+// Command contains the information of a consumed command.
 type Command struct {
 	Key    uuid.UUID       `json:"key,omitempty"`
 	ID     uuid.UUID       `json:"id"`
 	Action string          `json:"action"`
 	Data   json.RawMessage `json:"data"`
-}
-
-// NewEvent create a new event as a respond to the consumed command
-func (command *Command) NewEvent(action string, version int, key uuid.UUID, data []byte) *Event {
-	id := uuid.NewV4()
-	event := &Event{
-		Parent:       command.ID,
-		ID:           id,
-		Action:       action,
-		Data:         data,
-		Key:          key,
-		Acknowledged: true,
-		Version:      version,
-	}
-
-	return event
-}
-
-// NewErrorEvent creates a new error event as a respond to the command
-func (command *Command) NewErrorEvent(action string, data []byte) *Event {
-	id := uuid.NewV4()
-	key := uuid.Nil
-	event := &Event{
-		Parent:       command.ID,
-		ID:           id,
-		Action:       action,
-		Data:         data,
-		Key:          key,
-		Acknowledged: false,
-	}
-
-	return event
-}
-
-// Populate populate the command with the data from a kafka message
-func (command *Command) Populate(message *sarama.ConsumerMessage) error {
-	for _, header := range message.Headers {
-		switch string(header.Key) {
-		case ActionHeader:
-			command.Action = string(header.Value)
-		case IDHeader:
-			id, err := uuid.FromString(string(header.Value))
-
-			if err != nil {
-				return err
-			}
-
-			command.ID = id
-		}
-	}
-
-	id, err := uuid.FromString(string(message.Key))
-
-	if err != nil {
-		return err
-	}
-
-	command.Key = id
-	command.Data = message.Value
-
-	return nil
 }
--- event.go
+++ event.go
@@ -2,13 +2,12 @@ package commander
 
 import (
 	"encoding/json"
-	"strconv"
 
-	"github.com/Shopify/sarama"
 	uuid "github.com/satori/go.uuid"
 )
 
-// Event is produced as the result from a command
+// Event contains the information of a consumed event.
+// A event is produced as the result of a command.
 type Event struct {
 	Parent       uuid.UUID       `json:"parent"`
 	ID           uuid.UUID       `json:"id"`
@@ -17,57 +16,4 @@ type Event struct {
 	Key          uuid.UUID       `json:"key"`
 	Acknowledged bool            `json:"acknowledged"`
 	Version      int             `json:"version"`
-}
-
-// Populate the event with the data from the given kafka message
-func (event *Event) Populate(message *sarama.ConsumerMessage) error {
-	for _, header := range message.Headers {
-		switch string(header.Key) {
-		case ActionHeader:
-			event.Action = string(header.Value)
-		case ParentHeader:
-			parent, err := uuid.FromString(string(header.Value))
-
-			if err != nil {
-				return err
-			}
-
-			event.Parent = parent
-		case IDHeader:
-			id, err := uuid.FromString(string(header.Value))
-
-			if err != nil {
-				return err
-			}
-
-			event.ID = id
-		case AcknowledgedHeader:
-			acknowledged, err := strconv.ParseBool(string(header.Value))
-
-			if err != nil {
-				return err
-			}
-
-			event.Acknowledged = acknowledged
-		case VersionHeader:
-			version, err := strconv.ParseInt(string(header.Value), 10, 0)
-
-			if err != nil {
-				return err
-			}
-
-			event.Version = int(version)
-		}
-	}
-
-	id, err := uuid.FromString(string(message.Key))
-
-	if err != nil {
-		return err
-	}
-
-	event.Key = id
-	event.Data = message.Value
-
-	return nil
 }

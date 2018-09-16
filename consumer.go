217bbb140f89e70bc9aafa6b6a3f329b1b88a986
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-09-16 23:44:25 +0200

    Revert "refactoring: initialize topics in new constructor"
    This reverts commit 4e32fe49b7c479d1549e4888052df1b936bc0c60.

--- consumer.go
+++ consumer.go
@@ -7,21 +7,14 @@ import (
 	cluster "github.com/bsm/sarama-cluster"
 )
 
-// NewConsumer initalizes a new consumer struct with the given cluster client, group and topics.
-// Once the consumer is initialized is it not plausible to add more topics to the consumer.
-func NewConsumer(client *cluster.Client, group string, topics []string) (*Consumer, error) {
-	cluster, err := cluster.NewConsumerFromClient(client, group, topics)
-	if err != nil {
-		return nil, err
-	}
-
+// NewConsumer initalizes a new consumer struct with the given cluster client.
+func NewConsumer(client *cluster.Client, group string) *Consumer {
 	consumer := &Consumer{
-		Group:   group,
-		client:  client,
-		cluster: cluster,
+		Group:  group,
+		client: client,
 	}
 
-	return consumer, nil
+	return consumer
 }
 
 // Consumer this consumer consumes messages from a
@@ -38,12 +31,29 @@ type Consumer struct {
 	events    map[string][]chan *sarama.ConsumerMessage
 }
 
+// AddTopic adds the given topic to the subscribed topics map.
+// Topics can only be added before consuming.
+func (consumer *Consumer) AddTopic(topic string) {
+	consumer.Topics[topic] = []chan *sarama.ConsumerMessage{}
+}
+
 // Consume subscribes to all given topics and creates a new consumer.
 // The consumed messages get send to subscribed topic subscriptions.
 // A topic subscription could be made with the consumer.Subscribe method.
 // Before a message is passed on to any topic subscription is the BeforeEvent event emitted.
 // And once a event has been consumed and processed is the AfterEvent event emitted.
 func (consumer *Consumer) Consume() error {
+	topics := []string{}
+	for topic := range consumer.Topics {
+		topics = append(topics, topic)
+	}
+
+	cluster, err := cluster.NewConsumerFromClient(consumer.client, consumer.Group, topics)
+	if err != nil {
+		return err
+	}
+
+	consumer.cluster = cluster
 	for message := range consumer.cluster.Messages() {
 		consumer.EmitEvent(BeforeEvent, message)
 

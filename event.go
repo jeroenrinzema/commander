4ff61082c9e6185a3f026699d112c227a156fa6a
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-09-22 23:59:59 +0200

    feat: event headers

--- event.go
+++ event.go
@@ -9,11 +9,12 @@ import (
 // Event contains the information of a consumed event.
 // A event is produced as the result of a command.
 type Event struct {
-	Parent       uuid.UUID       `json:"parent"`
-	ID           uuid.UUID       `json:"id"`
-	Action       string          `json:"action"`
-	Data         json.RawMessage `json:"data"`
-	Key          uuid.UUID       `json:"key"`
-	Acknowledged bool            `json:"acknowledged"`
-	Version      int             `json:"version"`
+	Parent       uuid.UUID         `json:"parent"`
+	Headers      map[string]string `json:"headers"`
+	ID           uuid.UUID         `json:"id"`
+	Action       string            `json:"action"`
+	Data         json.RawMessage   `json:"data"`
+	Key          uuid.UUID         `json:"key"`
+	Acknowledged bool              `json:"acknowledged"`
+	Version      int               `json:"version"`
 }

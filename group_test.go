acc649464b6881a64fc664507711b4d344eac538
Jeroen Rinzema <post@jeroen.no>
2018-10-27 16:50:14 +0200

    feat: 70% tests milestone

--- group_test.go
+++ group_test.go
@@ -1,6 +1,7 @@
 package commander
 
 import (
+	"context"
 	"testing"
 	"time"
 
@@ -159,4 +160,95 @@ func TestProduceEvent(t *testing.T) {
 	event := group.NewEvent("testing", 1, parent, key, []byte("{}"))
 
 	group.ProduceEvent(event)
+}
+
+// TestEventConsumer test if events get consumed
+func TestEventConsumer(t *testing.T) {
+	group := NewTestGroup()
+	client, consumer, _ := NewTestClient(group)
+
+	go client.Consume()
+	go func() {
+		deadline := time.Now().Add(500 * time.Millisecond)
+
+		ctx, cancel := context.WithDeadline(context.Background(), deadline)
+		events, close := group.NewEventsConsumer()
+
+		defer cancel()
+		defer close()
+
+		select {
+		case <-events:
+		case <-ctx.Done():
+			t.Error("no message was consumed within the deadline")
+		}
+	}()
+
+	id := uuid.NewV4()
+	parent := uuid.NewV4()
+	key := uuid.NewV4()
+
+	message := NewMessage(key.String(), group.EventTopic, []byte("{}"), []kafka.Header{
+		kafka.Header{
+			Key:   ActionHeader,
+			Value: []byte("testing"),
+		},
+		kafka.Header{
+			Key:   AcknowledgedHeader,
+			Value: []byte("true"),
+		},
+		kafka.Header{
+			Key:   ParentHeader,
+			Value: []byte(parent.Bytes()),
+		},
+		kafka.Header{
+			Key:   IDHeader,
+			Value: []byte(id.String()),
+		},
+		kafka.Header{
+			Key:   VersionHeader,
+			Value: []byte("1"),
+		},
+	})
+
+	consumer.Emit(message)
+}
+
+// TestCommandConsumer test if commands get consumed
+func TestCommandConsumer(t *testing.T) {
+	group := NewTestGroup()
+	client, consumer, _ := NewTestClient(group)
+
+	go client.Consume()
+	go func() {
+		deadline := time.Now().Add(500 * time.Millisecond)
+
+		ctx, cancel := context.WithDeadline(context.Background(), deadline)
+		commands, close := group.NewCommandsConsumer()
+
+		defer cancel()
+		defer close()
+
+		select {
+		case <-commands:
+		case <-ctx.Done():
+			t.Error("no message was consumed within the deadline")
+		}
+	}()
+
+	id := uuid.NewV4()
+	key := uuid.NewV4()
+
+	message := NewMessage(key.String(), group.CommandTopic, []byte("{}"), []kafka.Header{
+		kafka.Header{
+			Key:   ActionHeader,
+			Value: []byte("testing"),
+		},
+		kafka.Header{
+			Key:   IDHeader,
+			Value: []byte(id.String()),
+		},
+	})
+
+	consumer.Emit(message)
 }

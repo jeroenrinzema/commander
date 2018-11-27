c6a2476cf7db73594cff551f4e78b4c5446c0e48
Jeroen Rinzema <post@jeroen.no>
2018-11-27 11:38:45 +0100

    feat: improved mock example

--- examples/mock/go.mod
+++ examples/mock/go.mod
@@ -4,3 +4,5 @@ require (
 	github.com/jeroenrinzema/commander v0.0.0-20181126160223-a09d37d1790b
 	github.com/satori/go.uuid v1.2.0
 )
+
+replace github.com/jeroenrinzema/commander => ../../
--- examples/mock/main.go
+++ examples/mock/main.go
@@ -10,6 +10,10 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+/**
+ * A commander group contains all the information needed for commander
+ * to setup it's consumers and producers.
+ */
 var group = &commander.Group{
 	Topics: []commander.Topic{
 		commander.Topic{
@@ -32,15 +36,28 @@ func main() {
 	connectionstring := ""
 	dialect := &commander.MockDialect{}
 
+	/**
+	 * When constrcuting a new commander instance do you have to construct a commander.Dialect as well.
+	 * A dialect consists mainly of a producer and a consumer that acts as a connector to the wanted infastructure.
+	 */
 	_, err := commander.New(dialect, connectionstring, group)
 	if err != nil {
 		panic(err)
 	}
 
+	/**
+	 * HandleFunc handles an "example" command. Once a command with the action "example" is
+	 * processed will a event with the action "created" be produced to the events topic.
+	 */
 	group.HandleFunc("example", commander.CommandTopic, func(writer commander.ResponseWriter, message interface{}) {
 		writer.ProduceEvent("created", 1, uuid.NewV4(), nil)
 	})
 
+	/**
+	 * Handle creates a new "example" command that is produced to the groups writable command topic.
+	 * Once the command is written is a responding event awaited. The responsing event has a header
+	 * with the parent id set to the id of the received command.
+	 */
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		command := commander.NewCommand("example", uuid.NewV4(), nil)
 		event, err := group.SyncCommand(command)

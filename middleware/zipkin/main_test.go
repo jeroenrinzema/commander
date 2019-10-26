7d145b83433f2975e9e3f67c1df44ad87bb807ac
Jeroen Rinzema <post@jeroen.no>
2019-10-26 21:40:30 +0200

    feat: increased zipkin tests

--- middleware/zipkin/main_test.go
+++ middleware/zipkin/main_test.go
@@ -3,14 +3,20 @@ package zipkin
 import (
 	"fmt"
 	"testing"
+
+	"github.com/jeroenrinzema/commander"
 )
 
-// TestNewZipkinMiddleware tests if able to construct a new zipkin middleware instance
-func TestNewZipkinMiddleware(t *testing.T) {
+func MockConn() string {
 	host := "https://example.com"
 	name := "mock"
 	conn := fmt.Sprintf("host=%s name=%s", host, name)
+	return conn
+}
 
+// TestNewZipkinMiddleware tests if able to construct a new zipkin middleware instance
+func TestNewZipkinMiddleware(t *testing.T) {
+	conn := MockConn()
 	zip, err := New(conn)
 	if err != nil {
 		t.Fatal(err)
@@ -19,4 +25,45 @@ func TestNewZipkinMiddleware(t *testing.T) {
 	if zip == nil {
 		t.Fatal("returned instance is nill")
 	}
+}
+
+func TestBeforeConsume(t *testing.T) {
+	conn := MockConn()
+	zip, err := New(conn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	message := commander.NewMessage("example", 1, nil, nil)
+	writer := commander.NewWriter(nil, nil)
+
+	next := func(message *commander.Message, writer commander.Writer) {}
+	zip.BeforeConsume(next)(message, writer)
+}
+
+func TestBeforeProduce(t *testing.T) {
+	conn := MockConn()
+	zip, err := New(conn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	message := commander.NewMessage("example", 1, nil, nil)
+
+	next := func(message *commander.Message) {}
+	zip.BeforeProduce(next)(message)
+}
+
+func TestBeforeProduceWithParent(t *testing.T) {
+	conn := MockConn()
+	zip, err := New(conn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	message := commander.NewMessage("example", 1, nil, nil)
+	writer := commander.NewWriter(nil, nil)
+
+	zip.BeforeConsume(func(message *commander.Message, writer commander.Writer) {})(message, writer)
+	zip.BeforeProduce(func(message *commander.Message) {})(message)
 }
